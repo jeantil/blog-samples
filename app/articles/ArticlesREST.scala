a9f1eb6e309ed12033f2911ad2b0e385ee89e07c
Jean Helou <jean.helou@gmail.com>
2015-02-15 18:05:18 +0100

    Improved exception handling

--- app/articles/ArticlesREST.scala
+++ app/articles/ArticlesREST.scala
@@ -5,13 +5,29 @@ import play.api.mvc.{Action, Controller}
 import play.api.libs.concurrent.Execution.Implicits._
 
 class ArticlesREST(val articleRepository: ArticleRepository) extends Controller {
+
+  def exception2Location(exception: Exception): String =
+    Option(exception.getStackTrace)
+      .flatMap(_.headOption)
+      .map(_.toString)
+      .getOrElse("unknown")
+
+  def jsonInternalServerError(msg: String, cause: Exception) = {
+    val jsonMsg = Json.obj(
+      "reason" -> msg,
+      "location" -> exception2Location(cause)
+    )
+    InternalServerError(jsonMsg)
+  }
+
+
   def get (id: String) = Action.async { implicit request =>
     val articleOptionFuture = articleRepository.findById (id)
     articleOptionFuture.map (articleOption =>
       articleOption.map (article =>
         Ok (Json.toJson (article) )
       ).getOrElse (NotFound (Json.obj ("reason" -> s"no article for $id") ) )
-    )
+    ).recover{ case e:Exception => jsonInternalServerError(e.getMessage, e)}
   }
 }
 object ArticlesREST extends ArticlesREST(new FakeArticleRepository())
