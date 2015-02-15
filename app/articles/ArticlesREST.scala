980a21d1280a3c882a1b36ef85f5806641c85c0f
Jean Helou <jean.helou@gmail.com>
2015-02-15 18:09:01 +0100

    Extract HTTP result mappers for the non exceptionnal cases

--- app/articles/ArticlesREST.scala
+++ app/articles/ArticlesREST.scala
@@ -6,6 +6,10 @@ import play.api.libs.concurrent.Execution.Implicits._
 
 class ArticlesREST(val articleRepository: ArticleRepository) extends Controller {
 
+  def jsonOk(article:Article)=Ok(Json.toJson(article))
+
+  def jsonNotFound(msg: String) = NotFound(Json.obj("reason" -> msg))
+
   def exception2Location(exception: Exception): String =
     Option(exception.getStackTrace)
       .flatMap(_.headOption)
@@ -24,9 +28,8 @@ class ArticlesREST(val articleRepository: ArticleRepository) extends Controller 
   def get (id: String) = Action.async { implicit request =>
     val articleOptionFuture = articleRepository.findById (id)
     articleOptionFuture.map (articleOption =>
-      articleOption.map (article =>
-        Ok (Json.toJson (article) )
-      ).getOrElse (NotFound (Json.obj ("reason" -> s"no article for $id") ) )
+      articleOption.map (article => jsonOk(article)
+      ).getOrElse ( jsonNotFound(s"no article for $id") )
     ).recover{ case e:Exception => jsonInternalServerError(e.getMessage, e)}
   }
 }
