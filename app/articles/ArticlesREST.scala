211f53183a265ad7a7b10fcc3b5cf4a689cf8b30
Jean Helou <jean.helou@gmail.com>
2015-02-15 18:16:36 +0100

    Extract mapping responsibility to collaborator

--- app/articles/ArticlesREST.scala
+++ app/articles/ArticlesREST.scala
@@ -1,37 +1,11 @@
 package articles
 
-import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
-import play.api.libs.concurrent.Execution.Implicits._
 
 class ArticlesREST(val articleRepository: ArticleRepository) extends Controller {
-
-  def jsonOk(article:Article)=Ok(Json.toJson(article))
-
-  def jsonNotFound(msg: String) = NotFound(Json.obj("reason" -> msg))
-
-  def exception2Location(exception: Exception): String =
-    Option(exception.getStackTrace)
-      .flatMap(_.headOption)
-      .map(_.toString)
-      .getOrElse("unknown")
-
-  def jsonInternalServerError(msg: String, cause: Exception) = {
-    val jsonMsg = Json.obj(
-      "reason" -> msg,
-      "location" -> exception2Location(cause)
-    )
-    InternalServerError(jsonMsg)
-  }
-
   def get(id: String) = Action.async { implicit request =>
     val articleOptionFuture = articleRepository.findById(id)
-    articleOptionFuture.map {
-      case Some(article) => jsonOk(article)
-      case None          => jsonNotFound(s"no article for $id")
-    }.recover {
-      case e: Exception => jsonInternalServerError(e.getMessage, e)
-    }
+    mvc.ResultMapper.toJsonResult(articleOptionFuture, s"no article for $id")
   }
 }
 object ArticlesREST extends ArticlesREST(new FakeArticleRepository())
