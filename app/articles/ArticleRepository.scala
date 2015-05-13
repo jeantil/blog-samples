228ae7bc3a7e33bf4499e8e444de116b2935dd39
Jean Helou <jean.helou@gmail.com>
2015-05-13 18:23:29 +0200

    Use FutureO in ArticleRepository

--- app/articles/ArticleRepository.scala
+++ app/articles/ArticleRepository.scala
@@ -1,24 +1,26 @@
 package articles
 
-import support.NotFoundException
+import support.{FutureO, NotFoundException}
 
 import scala.concurrent.Future
 
 trait ArticleRepository {
-  def findById(id: String): Future[Article]
+  def findById(id: String): FutureO[Article]
 }
+
 object ArticleRepository{
   case class ArticleNotFound(id:String) extends NotFoundException
 }
 
 class FakeArticleRepository extends ArticleRepository {
 
-  def findById(id: String): Future[Article] = {
-    id match {
-      case "0"      => Future.successful( Article("0", "good article", 10.0) )
-      case id @ "1" => Future.failed(ArticleRepository.ArticleNotFound(id))
+  override def findById(id: String): FutureO[Article] = {
+    val articleFO: Future[Option[Article]] = id match {
+      case "0"      => Future.successful( Option(Article("0", "good article", 10.0) ))
+      case id @ "1" => Future.successful(None)
       case "2"      => Future.failed(new java.io.IOException("Connection lost !!") )
     }
+    FutureO(articleFO)
   }
 }
 
