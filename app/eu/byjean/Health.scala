caef16bf2ed511c0b5d1d0b4231d6b619a1d19b9
Jean Helou <jean.helou@gmail.com>
2015-07-10 18:13:58 +0200

    buildinfo

--- app/eu/byjean/Health.scala
+++ app/eu/byjean/Health.scala
@@ -1,7 +1,20 @@
 package eu.byjean
 
-import play.api.mvc.Controller
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
+import play.api.libs.json.{JsString, JsValue, Writes, Json}
+import play.api.mvc.{Action, Controller}
 
 class Health extends Controller{
-  def check=TODO
+  val isoDateTimeWrites = new Writes[org.joda.time.DateTime] {
+    def writes(d: org.joda.time.DateTime): JsValue = JsString(d.toString(ISODateTimeFormat.dateTime()))
+  }
+  def check=Action { request =>
+    val json = Json.obj(
+      "version" -> BuildInfo.version,
+      "timestamp" -> Json.toJson(DateTime.now())(isoDateTimeWrites),
+      "reverse" -> routes.Health.check().absoluteURL(secure = true)(request)
+    )
+    Ok(json)
+  }
 }
--- build.sbt
+++ build.sbt
@@ -1,5 +1,10 @@
+import sbtbuildinfo.BuildInfoPlugin.autoImport._
+
 name := "ultimate-build"
 
 scalaVersion := "2.11.7"
 
-lazy val `ultimate-build` = (project in file(".")).enablePlugins(PlayScala)
+lazy val `ultimate-build` = (project in file(".")).enablePlugins(PlayScala, BuildInfoPlugin)
+
+buildInfoKeys := Seq[BuildInfoKey](name, version, scalaVersion, sbtVersion)
+buildInfoPackage := "eu.byjean"
